0cd122077012542ad962dfc42299f5435dc0b12b
Bogomil Dimitrov <bogomil.dimitrov@yahoo.com>
2014-10-30 16:28:57 +0200

    Added animation frame to main.js

--- JSGW-DATA/js/main.js
+++ JSGW-DATA/js/main.js
@@ -6,6 +6,31 @@ function initCanvas() {
     canvasHeight = ctx.canvas.height;
 }
 
+/*Initializing canvas on load*/
 window.addEventListener('load', function () {
     initCanvas();
 });
+
+/*Initializing cross-browser Animation Frame to start the animation drawing*/
+(function(){
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+var requestId = 0;
+
+function Start(){
+    requestId = window.requestAnimationFrame(Draw);
+}
+
+function Stop(){
+    if(requestId){
+        window.cancelAnimationFrame(requestId);
+    }
+    requestId = 0;
+}
+
+/*Starts animation*/
+Start();
