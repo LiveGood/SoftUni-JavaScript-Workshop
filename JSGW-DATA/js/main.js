2c0231e12aa68b94fef2c63600fe4d1bb5da6b7c
Bogomil Dimitrov <bogomil.dimitrov@yahoo.com>
2014-10-30 16:42:05 +0200

    modified animation frame to follow High-quality code

--- JSGW-DATA/js/main.js
+++ JSGW-DATA/js/main.js
@@ -1,25 +1,26 @@
-var ctx, canvasWidth, canvasHeight;
+var ctx,
+    canvasWidth,
+    canvasHeight,
+    requestId = 0;
 
 function initCanvas() {
     ctx = document.getElementById("canvas").getContext("2d");
     canvasWidth = ctx.canvas.width;
     canvasHeight = ctx.canvas.height;
+
+    /*Initializing cross-browser Animation Frame to start the animation drawing*/
+    window.requestAnimationFrame =
+        window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+    /*Starts animation*/
+    Start();
 }
 
 /*Initializing canvas on load*/
 window.addEventListener('load', function () {
     initCanvas();
 });
-
-/*Initializing cross-browser Animation Frame to start the animation drawing*/
-(function(){
-    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
-    window.requestAnimationFrame = requestAnimationFrame;
-})();
-
-var requestId = 0;
 
 function Start(){
     requestId = window.requestAnimationFrame(Draw);
@@ -32,5 +33,4 @@ function Stop(){
     requestId = 0;
 }
 
-/*Starts animation*/
-Start();
+
